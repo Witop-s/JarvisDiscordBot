183491474836c716847159c64fdbc3a6fdcefebb
Noah Nicolas <noahesteve.nicolas@gmail.com>
2024-04-10 23:11:20 -0400

    confirmation admin SMP

--- src/minecraft.py
+++ src/minecraft.py
@@ -59,10 +59,6 @@ class AcceptDenyView(View):
     async def deny(self, interaction: discord.Interaction, button: Button):
         await interaction.response.edit_message(content=f"User has been denied!", view=None)
 
-    @discord.ui.button(label="Annuler", style=discord.ButtonStyle.grey)
-    async def annuler(self, interaction: discord.Interaction, button: Button):
-        await interaction.response.edit_message(content="Action annulée", view=None)
-
 
 async def add_role_minecraft(interaction: discord.Interaction, role: discord.Role):
     # Get the member who clicked the button
@@ -80,28 +76,37 @@ async def edit_join_request(interaction: discord.Interaction, role: discord.Role
     # Get the member who clicked the button
     member = interaction.user
 
+    # If that is not the same member who made the request, refuse the request
+    if member.id != ctx.author.id:
+        await interaction.response.send_message("Ce n'est pas ta demande !", ephemeral=True)
+        return
+
     # Get the original message sent by the bot
     message = await ctx.channel.fetch_message(interaction.message.id)
     embed = discord.Embed(title="Join Request",
                           description=f"C'est tout bon {member.mention} ! Une fois ta demande acceptée, "
-                                      f"tu pourras rejoindre le serveur SMP sous le pseudo "
-                                      f"\"{message.embeds[0].title}\" en tant que {role.mention}.",
+                                      f"tu pourras rejoindre le serveur SMP avec le pseudo "
+                                      f"{pseudo_minecraft}, en tant que {role.mention}.",
                           color=discord.Color.blue())
     embed.set_thumbnail(url=skin_url)
     embed.set_footer(text="... En attente de validation ...")
     await message.edit(embed=embed, view=None)
+
+    await admin_confirm_join(ctx, pseudo_minecraft, role)  # Attendre la confirmation d'un admin
 
 
 async def join_request(ctx, pseudo_minecraft: str):
     error = False
 
     # Si le salon n'est pas le salon de présentation, on refuse la demande
-    if ctx.channel.id != salons["id_salon_film"]:
+    """
+    if ctx.channel.id != salons["id_salon_rejoindre_smp"]:
         # Réponse à l'utilisateur
         message = (f"{ctx.author.mention}, tu ne peux pas faire de demande de faction ici. Va dans le salon "
-                     f"{ctx.guild.get_channel(salons['id_salon_film']).mention} pour faire ta demande.")
+                   f"{ctx.guild.get_channel(salons['id_salon_rejoindre_smp']).mention} pour faire ta demande.")
         await ctx.interaction.response.send_message(message, ephemeral=True)
         return
+    """
 
     role_pacifiste = discord.utils.get(ctx.guild.roles, id=roles["pacifiste"])
     role_survivant = discord.utils.get(ctx.guild.roles, id=roles["survivant"])
@@ -111,8 +116,8 @@ async def join_request(ctx, pseudo_minecraft: str):
     message = (
         f"Salut {ctx.author.mention}, tu veux rejoindre le serveur SMP avec le pseudo \"{pseudo_minecraft}\"."
         f" Choisis ta faction ci-dessous pour commencer l'aventure !"
-        f"\n\n**Pacifiste** : PVP désactivé, possibilite de claim"
-        f"\n**Survivant** : PVP actif, chacun pour soi !\n")
+        f"\n\n**Pacifiste** : PVP désactivé, possibilité de claim"
+        f"\n**Survivant** : PVP actif, chacun pour soi !\n\n")
 
     message_footer = "Ce choix est définitif. Tu ne pourras pas changer de faction par la suite."
 
@@ -160,15 +165,35 @@ async def join_request(ctx, pseudo_minecraft: str):
     await ctx.send(embed=embed, view=view)
 
 
-async def confirm_join(ctx, pseudo_minecraft: str, faction: discord.Role):
-    skin_url = await get_player_skin(pseudo_minecraft)
-    message = (f"@{ctx.author.display_name} veux rejoindre le serveur SMP avec le pseudo Minecraft "
-               f"\"{pseudo_minecraft}\" et la faction {faction.mention}.")
+async def edit_admin_confirm_join(interaction: discord.Interaction, ctx, pseudo_minecraft: str, faction: discord.Role):
+    # Ici il faudra editer le message de confirmation admin, ainsi que le message original de la demande
+    return # TODO
+
+
+async def admin_confirm_join(ctx, pseudo_minecraft: str, faction: discord.Role):
+    skin_dict = await get_player_skin(pseudo_minecraft)
+    skin_url = skin_dict["url"]
+    message = (f"{ctx.author.mention} veux rejoindre le serveur SMP sous le pseudo "
+               f"{pseudo_minecraft} et la faction {faction.mention}.")
     embed = discord.Embed(title="Join Request", description=message, color=discord.Color.blue())
     embed.set_thumbnail(url=skin_url)
     embed.set_footer(text="Cliquer sur les boutons pour accepter ou refuser la demande.")
-    view = AcceptDenyView()
-    await ctx.send(embed=embed, view=view)
+    #view = AcceptDenyView()
+
+    view = discord.ui.View()
+    accept_button = discord.ui.Button(label="Accepter", style=discord.ButtonStyle.green)
+    accept_button.callback = lambda interaction: edit_admin_confirm_join(interaction, ctx, pseudo_minecraft, faction)
+    view.add_item(accept_button)
+
+    deny_button = discord.ui.Button(label="Refuser", style=discord.ButtonStyle.red)
+    deny_button.callback = lambda interaction: edit_admin_confirm_join(interaction, ctx, pseudo_minecraft, faction)
+    view.add_item(deny_button)
+
+    # Salon de validation des demandes
+    channel = ctx.guild.get_channel(salons["id_salon_join_requests"])
+    print(channel)
+    await channel.send(embed=embed, view=view)
+
 
 # @bot.command(description="Sends the bot's latency.")  # this decorator makes a slash command
 # async def ping(ctx):  # a slash command will be created with the name "ping"
